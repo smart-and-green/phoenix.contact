e36eaceb199ec399c02f756f60427de0a7faf626
sunjunwei11 <969831051@qq.com>
2014-12-16 16:27:11 +0800

    数据库和服务器脚本基本功能已经写的差不多了
    数据库加了记录每个用户每个星期数据和每个月数据的表格，数据库插入记录时可以自动更新相关的表格内容，可以返回当前星期目前的排名和数据，当前月份目前的排名和数据，上次锻炼的时间、数据，最近10次锻炼的记录，最近10星期锻炼的记录，最近10个月锻炼的记录。

--- runner-server/src/database/phoenix.sql
+++ runner-server/src/database/phoenix.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2014-12-12 19:14:46
+Date: 2014-12-16 16:17:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,41 +34,65 @@ CREATE TABLE `equipment_information` (
 DROP TABLE IF EXISTS `exercise_information`;
 CREATE TABLE `exercise_information` (
   `user_id` varchar(255) NOT NULL,
-  `equipment_id` varchar(255) DEFAULT NULL,
-  `start_time` datetime DEFAULT NULL,
-  `end_time` datetime DEFAULT NULL,
-  `exercise_time` time DEFAULT NULL,
-  `calories` float DEFAULT NULL,
-  `power_generate` float DEFAULT NULL
+  `equipment_id` varchar(255) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `duration_time` time NOT NULL DEFAULT '00:00:00',
+  `energy` float DEFAULT '0',
+  `num` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of exercise_information
 -- ----------------------------
-INSERT INTO `exercise_information` VALUES ('jim', null, null, null, null, null, null);
+INSERT INTO `exercise_information` VALUES ('jim', '32', '2014-12-14 12:12:12', '2014-12-14 12:12:15', '00:00:03', '123', '1');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '23:23:22', '234', '2');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '23:23:22', '234', '3');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '23:23:22', '234', '4');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '23:23:22', '234', '5');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '23:23:22', '234', '6');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '00:00:01', '1', '7');
+INSERT INTO `exercise_information` VALUES ('jim', 'bike', '2014-12-12 10:10:10', '2014-12-12 10:11:11', '00:00:01', '1', '8');
+
+-- ----------------------------
+-- Table structure for month_information
+-- ----------------------------
+DROP TABLE IF EXISTS `month_information`;
+CREATE TABLE `month_information` (
+  `user_id` varchar(256) NOT NULL,
+  `year` int(11) DEFAULT '0',
+  `month` int(11) DEFAULT '0',
+  `month_energy` float(11,1) DEFAULT '0.0',
+  `month_time` time DEFAULT '00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of month_information
+-- ----------------------------
+INSERT INTO `month_information` VALUES ('jim', '2014', '12', '1643.0', '160:23:33');
+INSERT INTO `month_information` VALUES ('jim', '2015', '1', '23.0', '00:00:03');
+INSERT INTO `month_information` VALUES ('jim', '2015', '2', '32.0', '00:00:34');
 
 -- ----------------------------
 -- Table structure for total_information
 -- ----------------------------
 DROP TABLE IF EXISTS `total_information`;
 CREATE TABLE `total_information` (
-  `user_id` varchar(255) DEFAULT NULL,
-  `duration-summary` int(6) DEFAULT NULL,
-  `energy-summary` float(11,0) DEFAULT NULL,
-  `energy-rank-summary` int(11) DEFAULT NULL,
-  `duration-average` int(6) DEFAULT NULL,
-  `energy-average` float(11,0) DEFAULT NULL,
-  `energy-rank-average` int(11) DEFAULT NULL,
-  `duration-lastweek` int(6) DEFAULT NULL,
-  `energy-lastweek` float(11,0) DEFAULT NULL,
-  `energy-rank-lastweek` int(11) DEFAULT NULL
+  `user_id` varchar(255) NOT NULL,
+  `duration_summary` int(6) unsigned DEFAULT '0',
+  `energy_summary` float(11,0) unsigned DEFAULT '0',
+  `energy_rank-summary` int(11) DEFAULT '0',
+  `duration_average` int(6) DEFAULT '0',
+  `energy_average` float(11,1) DEFAULT '0.0',
+  `energy_rank_average` int(11) DEFAULT '0',
+  `exercise_number` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of total_information
 -- ----------------------------
-INSERT INTO `total_information` VALUES ('jim', '145', '23', '32', '224', '34', '54', '345', '23', '64');
-INSERT INTO `total_information` VALUES ('lucy', null, null, null, null, null, null, null, null, null);
+INSERT INTO `total_information` VALUES ('jim', '145', '1429', '32', '224', '357.2', '54', '4');
+INSERT INTO `total_information` VALUES ('lucy', '213', '22', '32', '32', '23.0', '12', '3');
 
 -- ----------------------------
 -- Table structure for user_information
@@ -100,3 +124,42 @@ CREATE TABLE `user_login` (
 -- ----------------------------
 INSERT INTO `user_login` VALUES ('jim', '123456');
 INSERT INTO `user_login` VALUES ('lucy', '123456');
+
+-- ----------------------------
+-- Table structure for week_information
+-- ----------------------------
+DROP TABLE IF EXISTS `week_information`;
+CREATE TABLE `week_information` (
+  `user_id` varchar(256) NOT NULL,
+  `year` int(11) DEFAULT '0',
+  `week` int(11) DEFAULT NULL,
+  `week_energy` float(11,1) DEFAULT '0.0',
+  `week_time` time DEFAULT '00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of week_information
+-- ----------------------------
+INSERT INTO `week_information` VALUES ('jim', '2014', '49', '3.0', '00:00:03');
+INSERT INTO `week_information` VALUES ('jim', '2014', '50', '3.0', '00:00:08');
+INSERT INTO `week_information` VALUES ('jim', '2014', '51', '4.0', '00:00:09');
+INSERT INTO `week_information` VALUES ('jim', '2015', '1', '3.0', '00:00:03');
+INSERT INTO `week_information` VALUES ('jim', '2015', '2', '2.0', '00:03:00');
+DROP TRIGGER IF EXISTS `ADD_DATA`;
+DELIMITER ;;
+CREATE TRIGGER `ADD_DATA` AFTER INSERT ON `exercise_information` FOR EACH ROW begin
+DECLARE total FLOAT DEFAULT 0;
+DECLARE cunt FLOAT DEFAULT 0;
+DECLARE number FLOAT DEFAULT 0;
+DECLARE num FLOAT DEFAULT 0;
+SELECT NEW.energy INTO cunt ;
+SELECT energy_summary INTO total FROM total_information WHERE (user_id = NEW.user_id);
+SET total = total + cunt;
+UPDATE total_information SET energy_summary=total WHERE (user_id= NEW.user_id);
+SELECT exercise_number INTO number FROM total_information WHERE (user_id= NEW.user_id);
+SET number = number + 1;
+UPDATE total_information SET exercise_number=number WHERE (user_id= NEW.user_id);
+UPDATE total_information SET energy_average=(total/number) WHERE (user_id= NEW.user_id);
+end
+;;
+DELIMITER ;
--- runner-server/src/services.py
+++ runner-server/src/services.py
@@ -66,11 +66,138 @@ def login(db):
                         ret["userdata"]["average"]["energy"] = k[5]
                         ret["userdata"]["average"]["globalRank"] = k[6]
                         
-                        ret["userdata"]["lastWeekSummary"] = {}
-                        ret["userdata"]["lastWeekSummary"]["duration"] = k[7]
-                        ret["userdata"]["lastWeekSummary"]["energy"] = k[8]
-                        ret["userdata"]["lastWeekSummary"]["globalRank"] = k[9]
-                        print ret                
+#                        ret["userdata"]["lastWeekSummary"] = {}
+#                        ret["userdata"]["lastWeekSummary"]["duration"] = k[7]
+#                        ret["userdata"]["lastWeekSummary"]["energy"] = k[8]
+#                        ret["userdata"]["lastWeekSummary"]["globalRank"] = k[9]
+                        
+                    cr.execute('''SELECT COUNT(*) FROM total_information WHERE energy_summary>(SELECT energy_summary FROM total_information WHERE 
+                                user_id=%(phoenix.user_id)s)''',{"phoenix.user_id":userid})
+                    summary_rank=cr.fetchall()
+                    summary_rank = summary_rank[0][0] + 1
+                        
+                    cr.execute('''SELECT COUNT(*) FROM total_information WHERE energy_average>(SELECT energy_average FROM total_information WHERE 
+                                  user_id=%(phoenix.user_id)s)''',{"phoenix.user_id":userid})
+                    average_rank=cr.fetchall()
+                    average_rank = average_rank[0][0] + 1
+                        
+                    now_time = "2015-11-15 10:12:56"
+                    cr.execute('''SELECT YEAR(%(now_time)s)''',{"now_time":now_time})
+                    year = cr.fetchall()
+                    year = year[0][0]#得到当前的年份
+                    print "当前的年份:",year 
+    
+                    cr.execute('''SELECT MONTH(%(now_time)s)''',{"now_time":now_time})
+                    month = cr.fetchall()
+                    month = month[0][0]#得到当前的月份
+                    print "当前的月份:",month  
+
+                    cr.execute('''SELECT WEEK(%(now_time)s,2)''',{"now_time":now_time})
+                    week = cr.fetchall()
+                    week = week[0][0]#得到这是今年第几个星期
+                    print "第几个星期:",week
+                    
+#---------------------------本月目前的排名---------------------------------------------------------------------------
+                    cr.execute('''SELECT month_energy FROM month_information WHERE(user_id=%(user_id)s AND year=%(year)s AND month=%(month)s)''',
+                                {"user_id":userid,"year":year,"month":month})
+                    month_energy = cr.fetchall()
+                    if (month_energy):  #本月有产生能量    
+                        print "能量元组",month_energy
+                        month_energy = month_energy[0][0]
+                        print "本月产生的能量:",month_energy             
+                        cr.execute('''SELECT COUNT(*) FROM month_information WHERE (month_energy>%(month_energy)s AND year=%(year)s AND month=%(month)s) ''',
+                                   {"month_energy":month_energy,"year":year,"month":month})
+                        month_rank = cr.fetchall()
+                        month_rank = month_rank[0][0]+1 
+                        print "本月目前排名:",month_rank 
+                    else:     #本月无产生能量
+                        cr.execute('''SELECT COUNT(*) FROM month_information WHERE (month_energy>0 AND year=%(year)s AND month=%(month)s) ''',
+                                   {"year":year,"month":month})
+                        month_rank = cr.fetchall()
+                        month_rank = month_rank[0][0]+1  
+                        print "本月没有锻炼目前排名:",month_rank 
+#-------------------------------------------------------------------------------------------------------                 
+
+#-----------------------本星期目前的排名-------------------------------------------------------------------
+                    cr.execute('''SELECT week_energy FROM week_information WHERE(user_id=%(user_id)s AND year=%(year)s AND week=%(week)s)''',
+                                {"user_id":userid,"year":year,"week":week})
+                    week_energy = cr.fetchall()
+                    if (week_energy):  #本星期有产生能量     
+                        print "能量元组",week_energy
+                        week_energy = week_energy[0][0]
+                        print "本星期产生的能量:",week_energy             
+                        cr.execute('''SELECT COUNT(*) FROM week_information WHERE (week_energy>%(week_energy)s AND year=%(year)s AND week=%(week)s) ''',
+                                   {"week_energy":week_energy,"year":year,"week":week})
+                        week_rank = cr.fetchall()
+                        week_rank = week_rank[0][0]+1 
+                        print "本星期目前排名:",week_rank 
+                    else:     #本星期无产生能量
+                        cr.execute('''SELECT COUNT(*) FROM week_information WHERE (week_energy>0 AND year=%(year)s AND week=%(week)s) ''',
+                                   {"year":year,"week":week})
+                        week_rank = cr.fetchall()
+                        print week_rank
+                        if (week_rank):
+                            week_rank = week_rank[0][0]+1
+                        else:                  
+                            week_rank = 1  
+                        print "本星期没有锻炼,本星期目前排名:",week_rank
+#-------------------------------------------------------------------------------------------------------
+
+#----------------------最后10条锻炼记录----------------------------------------------------------------------
+                    cr.execute('''SELECT COUNT(*) FROM exercise_information WHERE (user_id=%(user_id)s)''',{"user_id":userid})
+                    num = cr.fetchall()
+                    num = num[0][0]
+                    print "该用户在表中一共的记录条数:",num
+                    cr.execute('''SELECT * FROM exercise_information WHERE(user_id=%(user_id)s AND num>(%(num1)s-10) AND num<=(%(num2)s))''',
+                                {"user_id":userid,"num1":num,"num2":num})
+                    last_10_history = cr.fetchall()
+                    print "最近10条历史记录:",last_10_history                    
+#------------------------------------------------------------------------------------------------------ 
+
+#---------------------------10个星期的历史记录---------------------------------------------------------------                  
+                    if (week>=10):
+                        cr.execute('''SELECT * FROM week_information WHERE(user_id=%(user_id)s AND year=%(year)s AND week>(%(week1)s-10) AND week<=(%(week2)s))''',
+                                    {"user_id":userid,"year":year,"week1":week,"week2":week})
+                        week_10_history = cr.fetchall()
+                        print "最近10个星期历史记录:",week_10_history
+                    elif (1<=week<10): 
+                        cr.execute('''SELECT * FROM week_information WHERE(user_id=%(user_id)s AND year=%(year)s AND week>0 AND week<=(%(week)s))''',
+                                    {"user_id":userid,"year":year,"week":week})
+                        week_10_history1 = cr.fetchall()
+                        print "最近10个星期的记录，今年的:",week_10_history1
+                        
+                        week_lastyear = 10-week
+                        cr.execute('''SELECT * FROM week_information WHERE(user_id=%(user_id)s AND year=(%(year)s-1) AND week>(52-%(week_lastyear)s) AND week<=52)''',
+                                    {"user_id":userid,"year":year,"week_lastyear":week_lastyear})                        
+                        week_10_history2 = cr.fetchall()
+                        print "最近10个星期的记录，去年的:",week_10_history2
+                    else:
+                        pass                                                          
+#------------------------------------------------------------------------------------------------------ 
+
+#----------------------------10个月的历史记录-------------------------------------------------------------
+                    if (month>=10):
+                        cr.execute('''SELECT * FROM month_information WHERE(user_id=%(user_id)s AND year=%(year)s AND month>(%(month1)s-10) AND month<=(%(month2)s))''',
+                                    {"user_id":userid,"year":year,"month1":month,"month2":month})
+                        month_10_history = cr.fetchall()
+                        print "最近10个月历史记录:",month_10_history
+                    elif (1<=month<10): 
+                        cr.execute('''SELECT * FROM month_information WHERE(user_id=%(user_id)s AND year=%(year)s)''',
+                                    {"user_id":userid,"year":year})
+                        month_10_history1 = cr.fetchall()
+                        print "最近10个月的记录，今年的:",week_10_history1
+                        
+                        month_lastyear = 10-month
+                        cr.execute('''SELECT * FROM month_information WHERE(user_id=%(user_id)s AND year=(%(year)s-1) AND month>(12-%(month_lastyear)s) AND month<=12)''',
+                                    {"user_id":userid,"year":year,"month_lastyear":month_lastyear})                        
+                        month_10_history2 = cr.fetchall()
+                        print "最近10个月的记录，去年的:",month_10_history2
+                    else:
+                        pass  
+#------------------------------------------------------------------------------------------------------                                                                  
+                    print "总能量排名:",summary_rank
+                    print "平均能量排名:",average_rank
+                    print ret                
     cr.close()
     return ret
 
@@ -120,7 +247,80 @@ def uploadExRecord(db):
     print exerciseData
     print equipmentid
 
+@app.route('/data')#上传健身数据到数据库
+def data(db):
+    user_id = 'jim'
+    equipment_id = 'bike'
+    start_time = "2014-12-12 10:10:10"
+    end_time = 20141212101111
+    duration_time = "000001"
+    energy = 1
+    cr = db.cursor()
     
+    cr.execute('''SELECT YEAR(%(start_time)s)''',{"start_time":start_time})
+    year = cr.fetchall()
+    year = year[0][0]#得到当前的年份
+    print "当前的年份:",year 
+    
+    cr.execute('''SELECT MONTH(%(start_time)s)''',{"start_time":start_time})
+    month = cr.fetchall()
+    month = month[0][0]#得到当前的月份
+    print "当前的月份:",month  
+
+    cr.execute('''SELECT WEEK(%(start_time)s,2)''',{"start_time":start_time})
+    week = cr.fetchall()
+    week = week[0][0]#得到这是今年第几个星期
+    print "第几个星期:",week
+    
+#-----------------将本次运动的数据存储到数据库--------------------------------------------------------------------
+    cr.execute('''SELECT COUNT(*) FROM exercise_information WHERE (user_id=%(user_id)s)''',{"user_id":user_id})
+    num = cr.fetchall()
+    num = num[0][0]
+    print num #该用户之前一共有多少条记录
+    num = num + 1
+    cr.execute('''INSERT INTO exercise_information (user_id,equipment_id,start_time,end_time,duration_time,energy,num) 
+               VALUES (%s,%s,%s,%s,%s,%s,%s)''',(user_id,equipment_id,start_time,end_time,duration_time,energy,num))
+#----------------------------------------------------------------------------------------------------------
+
+#-----------------记录每个用户每月的数据记录 ----------------------------------------------------------------   
+    cr.execute('''SELECT COUNT(*) FROM month_information WHERE (user_id=%(user_id)s AND year=%(year)s AND month=%(month)s)''',
+                  {"user_id":user_id,"year":year,"month":month})
+    number = cr.fetchall()
+    number = number[0][0]#检查数据库中是否有这个月的记录，1表示有，0表示没有，若没有则创建这个月的记录，有则更新
+    print "检查数据库中是否有这个月的记录:",number
+    if(number == 0):
+        cr.execute('''INSERT INTO month_information (user_id,year,month,month_energy,month_time) VALUES (%s,%s,%s,%s,%s)''',
+                      (user_id,year,month,energy,duration_time))
+        print "生成本月数据记录成功"
+    elif (number == 1):
+        cr.execute('''UPDATE month_information SET month_energy=month_energy+%(energy)s,month_time=month_time+%(time)s WHERE 
+                    (user_id=%(user_id)s AND year=%(year)s AND month=%(month)s) ''',{"energy":energy,"time":duration_time,"user_id":user_id,"year":year,"month":month})
+        print "更新本月数据成功"
+    else:
+        pass
+#-------------------------------------------------------------------------------------------------------
+
+#---------------记录每个用户每星期的数据记录-------------------------------------------------------------------
+    cr.execute('''SELECT COUNT(*) FROM week_information WHERE (user_id=%(user_id)s AND year=%(year)s AND week=%(week)s)''',
+                  {"user_id":user_id,"year":year,"week":week})
+    number = cr.fetchall()
+    number = number[0][0]#检查数据库中是否有这个月的记录，1表示有，0表示没有，若没有则创建这个月的记录，有则更新
+    print "检查数据库中是否有这个星期的记录:",number
+    if(number == 0):
+        cr.execute('''INSERT INTO week_information (user_id,year,week,week_energy,week_time) VALUES (%s,%s,%s,%s,%s)''',
+                      (user_id,year,week,energy,duration_time))
+        print "生成这个星期数据记录成功"
+    elif (number == 1):
+        cr.execute('''UPDATE week_information SET week_energy=week_energy+%(energy)s,week_time=week_time+%(time)s WHERE 
+                    (user_id=%(user_id)s AND year=%(year)s AND week=%(week)s) ''',{"energy":energy,"time":duration_time,"user_id":user_id,"year":year,"week":week})
+        print "更新这个星期数据成功"
+    else:
+        pass
+#-------------------------------------------------------------------------------------------------
+            
+    print "提交成功"
+    return "提交成功了！！~~"
+    cr.close()
 
 session_opts = {
     'session.type': 'file',
