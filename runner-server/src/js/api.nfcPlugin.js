9a25ff38d27e4b296ebf1df13e2776405dfc95cd
adolli <404826140@qq.com>
2014-12-18 23:05:01 +0800

    增加js脚本nfcPlugin

---
+++ runner-server/src/js/api.nfcPlugin.js
@@ -0,0 +1,34 @@
+/*global PhoneGap*/
+
+var Nfc = function() {
+
+};
+
+Nfc.prototype = {  
+          
+    read: function(success, error, password){  
+        PhoneGap.exec(success, error, "NfcPlugin", "read", [password]);  
+    },
+
+    // @param blockAndDataList is an json object
+    // structure as below 
+    // [{
+    //     blockIndex: 1,
+    //     data: [0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x1, 0x1]
+    //  },
+    //  {
+    //     blockIndex: 2,
+    //     data: [0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1]
+    //  }]
+    write: function(success, error, password, blockAndDataList) {
+        PhoneGap.exec(success, error, "NfcPlugin", "write", [password, blockAndDataList]);  
+    }
+}; 
+
+PhoneGap.addConstructor(function() {  
+    if (!window.plugins) {
+    	window.plugins = { };
+    }
+    window.plugins.nfc = new Nfc(); 
+});  
+
