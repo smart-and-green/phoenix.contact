72e57ee450663749ff0af586fed42f4497eef698
adolli <404826140@qq.com>
2014-12-29 14:35:12 +0800

    app的返回按钮功能为应用程序最小化，页面返回则在app左上角（一般来说）。

--- runner/src/phoenix/contact/mobile/MainActivity.java
+++ runner/src/phoenix/contact/mobile/MainActivity.java
@@ -2,16 +2,27 @@ package phoenix.contact.mobile;
 
 import org.apache.cordova.DroidGap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
-public class MainActivity extends DroidGap
-{
+public class MainActivity extends DroidGap {
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//super.loadUrl("file:///android_asset/www/login.html");
+		// super.loadUrl("file:///android_asset/www/login.html");
 		super.loadUrl("http://10.141.90.240:8080/index");
 	}
-}
 
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+			moveTaskToBack(false);
+//			Intent intent = new Intent();
+//			intent.setAction("android.intent.action.MAIN");
+//			intent.addCategory("android.intent.category.HOME");
+//			startActivity(intent);
+		}
+		return false;
+	}
+}
